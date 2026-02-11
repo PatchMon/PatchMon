91b78d10a7b5e70b3afd37abc04cf770d26db26e
Muhammad Ibrahim <iby@9.technology>
2026-02-11 16:39:26 +0000

    Package version fix

--- agent-source-code/internal/pkgversion/version.go
+++ agent-source-code/internal/pkgversion/version.go
@@ -1,5 +1,5 @@
-// Package version provides version information for the agent
-package version
+// Package pkgversion provides version information for the agent
+package pkgversion
 
 // Version represents the current version of the patchmon-agent
 const Version = "1.4.0"
